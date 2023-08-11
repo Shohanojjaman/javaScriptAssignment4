b50fe3111083a01bcef326fdc0a6f452eabdc6f2
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2023-08-11 10:31:28 +0600

    Added validation on problem 3.

--- problem2.js
+++ problem2.js
@@ -5,6 +5,9 @@
  * @returns {boolean} True / False
  */
 function matchFinder(string1, string2) {
+  if (typeof string1 !== 'string' || typeof string2 !== 'string') {
+    return 'Please provide me string.';
+  }
   let match = string1.includes(string2);
   return match;
 }
